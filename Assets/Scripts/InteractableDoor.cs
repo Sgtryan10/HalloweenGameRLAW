5dc5e84d5b4fe1af168c690f29440edff8cae21e
Sgtryan10 <sgtryan123456@gmail.com>
2025-10-30 06:48:33 -0400

    door fix and fog

--- Assets/Scripts/InteractableDoor.cs
+++ Assets/Scripts/InteractableDoor.cs
@@ -3,9 +3,11 @@ public class InteractableDoor : MonoBehaviour
 {
     private Animator animator;
     private AudioSource audioSource;
+    private Camera mainCamera;
 
     public AudioClip openSound;
     public AudioClip closeSound;
+    public float interactionDistance = 3.0f;
 
     private bool isOpen = false;
     private const string IS_OPEN_PARAM = "IsOpen";
@@ -14,16 +16,27 @@ public class InteractableDoor : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+        mainCamera = Camera.main;
     }
 
-    void OnMouseDown()
+    void Update()
     {
-        ToggleDoor();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, interactionDistance))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    ToggleDoor();
+                }
+            }
+        }
     }
 
     public void ToggleDoor()
     {
-
         isOpen = !isOpen;
 
         if (isOpen)
@@ -38,3 +51,4 @@ public class InteractableDoor : MonoBehaviour
         animator.SetBool(IS_OPEN_PARAM, isOpen);
     }
 }
+
